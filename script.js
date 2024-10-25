52e1a7b510e86917c9a72720cd1507142b03964b
krissyjames <kristina.ashley.cook@gmail.com>
2024-10-25 17:11:05 -0400

    completion of task 2, refactor task one to stringify state name

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
-console.log("testing");
+// console.log("testing");
 function getStates() {
     return fetch('states.json')
     .then(response => response.json())
 }
-let test;
+
 getStates().then(states => {
     for (let i = 0; i < states.states.length; i++) {
         const stateArr = states.states;
-        const halfArrNum = Math.ceil(stateArr.length / 2)
+        const halfArrNum = Math.ceil(stateArr.length / 2);
         // console.log(halfArrNum);
         const halfArr = stateArr.slice(0, halfArrNum);
+        const secondHalfArr = stateArr.slice(halfArrNum)
+        // console.log(secondHalfArr);
         // console.log(halfArr);
         const stateDetails = halfArr[i];
         const stateName = halfArr[i].name;
-        // console.log(stateName);
+        const stateNameString = JSON.stringify(stateName);
+        // console.log(stateNameString);
         delete stateDetails.name;
-        // console.log(test);
         myStringDetails = JSON.stringify(stateDetails);
-        console.log(stateName, myStringDetails)
-        localStorage.setItem(stateName, myStringDetails);
+        // console.log(stateNameString, myStringDetails)
+        localStorage.setItem(stateNameString, myStringDetails);
+        const stateDetails2 = secondHalfArr[i];
+        const capitalName = secondHalfArr[i].capital;
+        // console.log(capitalName);
+        delete stateDetails2.capital;
+        // console.log(stateDetails2);
+        myStringDetails2 = JSON.stringify(stateDetails2);
+        myLowerCaseString = myStringDetails2.toLowerCase();
+        myCapitalNameString = JSON.stringify(capitalName);
+        // console.log(myCapitalNameString);
+        myLowerCaseCapital = myCapitalNameString.toLowerCase();
+        console.log(myLowerCaseCapital, myLowerCaseString);
+        sessionStorage.setItem(myLowerCaseCapital, myLowerCaseString);
     }
 })
